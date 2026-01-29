d44a469f6bec410e5a912b8e186f07331367cedb
Manh Hung <manhhunghm13032005@gmail.com>
2026-01-29 21:48:28 +0700

    update lần cuối

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,21 +1,22 @@
-// src/api/axiosClient.js
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:5000", // hoặc backend bạn
-  // ... các config khác
+  baseURL: "http://localhost:5000", // chỉnh theo backend bạn
+  withCredentials: false,
 });
 
-// Gắn token vào mỗi request nếu có
+// Interceptor: trước mỗi request, gắn token mới nhất từ localStorage
 axiosClient.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
+    } else {
+      delete config.headers.Authorization;
     }
     return config;
   },
-  (error) => Promise.reject(error)
+  (error) => Promise.reject(error),
 );
 
 export default axiosClient;
